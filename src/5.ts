cca89c4cd976fd87f3a7d8affb4ba601423e568b
Raph <rsuttiyotin@seek.com.au>
2021-09-08 14:13:38 +1000

    5

---
+++ src/5.ts
@@ -0,0 +1,16 @@
+// There is no type safety with any type
+let age: number;
+age = "12";
+// ~~~ Type '"12"' is not assignable to type 'number'
+
+age = "12" as any; // OK
+age += 1; // OK; at runtime, age is now "121"
+
+// There are no language services for any types
+interface Person {
+  first: string;
+  last: string;
+}
+
+const formatName = (p: Person) => `${p.first} ${p.last}`;
+const formatNameAny = (p: any) => `${p.first} ${p.last}`;
