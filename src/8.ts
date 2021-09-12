6562e28c7974db7c8d8c33cfddbddd2acb4202d7
Raph <rsuttiyotin@seek.com.au>
2021-09-12 10:30:38 +1000

    8

---
+++ src/8.ts
@@ -0,0 +1,60 @@
+let dummy: number = 12; // Don't do this
+let tummy = 12; // Type has been inferred
+
+// Complex object inferred
+const Partner = {
+  name: "Ryanair International",
+  partnerTier: "SUPER_PREMIUM" as const,
+  headOfficeAddress: {
+    addressLine1: "60 Cremorne street",
+    addressLine2: undefined,
+    suburb: "Cremorne",
+  },
+};
+
+// Arrays
+const square = (nums: number[]) => nums.map((x) => x * x);
+const squares = square([1, 2, 3, 4]); // Type is number[]
+
+// Preciseness
+const bummy: string = "x"; // Type is string
+const gummy = "y"; // Type is "y"
+let yummy = "z"; // Type is string
+
+// Help when refactoring
+interface PartnerInput {
+  uuid: number; // This will be refactored to string
+  name: string;
+}
+
+const logPartner = (partner: PartnerInput) => {
+  const uuid: number = partner.uuid;
+  const name: string = partner.name;
+  console.log(uuid, name);
+};
+
+const InferredLogPartner = (partner: PartnerInput) => {
+  const uuid = partner.uuid;
+  const name = partner.name;
+  console.log(uuid, name);
+};
+
+const DRYLogPartner = (partner: PartnerInput) => {
+  const { uuid, name } = partner;
+  console.log(uuid, name);
+};
+
+// When to explicitly annotate type
+const JobAdder = {
+  name: "JobAdder",
+  uuid: "202da638-7432-41f4-8c02-c7dc53f73ea5",
+};
+logPartner(JobAdder);
+// ~~~~~ Argument .. is not assignable to parameter of type 'PartnerInput'
+//         Types of property 'uuid' are incompatible
+//         Type 'string' is not assignable to type 'number'
+const Broadbean: PartnerInput = {
+  name: "Furby",
+  uuid: "d4681fce-d913-4aab-b929-e253abc889f2", // Catch this early
+};
+logPartner(Broadbean);
