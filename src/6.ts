9fff538177bd531619fbcffe44068cbf9f51732b
Raph <rsuttiyotin@seek.com.au>
2021-10-22 08:02:29 +1100

    Init

--- src/6.ts
+++ src/6.ts
@@ -1,58 +1,143 @@
-// Syntax
-type TState = {
-  name: string;
-  capital: string;
-};
-interface IState {
-  name: string;
-  capital: string;
+// Present invalid states mixed
+interface State {
+  pageText: string;
+  isLoading: boolean;
+  error?: string;
 }
 
-// Index signature
-type TDict = { [key: string]: string };
-interface IDict {
-  [key: string]: string;
+declare let currentPage: string;
+function renderPage(state: State) {
+  if (state.error) {
+    return `Error! Unable to load ${currentPage}: ${state.error}`;
+  } else if (state.isLoading) {
+    return `Loading ${currentPage}...`;
+  }
+  return `<h1>${currentPage}</h1>\n${state.pageText}`;
 }
 
-// Function types
-type TFn = (x: number) => string;
-interface IFn {
-  (x: number): string;
+function getUrlForPage(p: string) {
+  return "";
 }
-const toStrT: TFn = (x) => "" + x; // OK
-const toStrI: IFn = (x) => "" + x; // OK
-
-// Generics
-type TPair<T> = {
-  first: T;
-  second: T;
-};
-interface IPair<T> {
-  first: T;
-  second: T;
+async function changePage(state: State, newPage: string) {
+  state.isLoading = true;
+  try {
+    const response = await fetch(getUrlForPage(newPage));
+    if (!response.ok) {
+      throw new Error(`Unable to load ${newPage}: ${response.statusText}`);
+    }
+    const text = await response.text();
+    state.isLoading = false;
+    state.pageText = text;
+  } catch (e) {
+    state.error = "" + e;
+  }
 }
 
-// extend each other
-interface IStateWithProp extends TState {
-  population: number;
-} // but not complex types eg. union
-type UState = { address: string } | { addresses: string };
-interface IStateWithProp extends UState {
-  population: number;
+// Fix
+interface RequestPending {
+  state: "pending";
 }
-type TStateWithProp = IState & { population: number };
-type TStateWithProp2 = UState & { population: number };
+interface RequestError {
+  state: "error";
+  error: string;
+}
+interface RequestSuccess {
+  state: "ok";
+  pageText: string;
+}
+type RequestState = RequestPending | RequestError | RequestSuccess;
 
-// Interface can be augmented
-interface IState {
-  name: string;
-  capital: string;
-} // In a library
-interface IState {
-  population: number;
-} // In our code base
-const victoria: IState = {
-  name: "Victoria",
-  capital: "Melbourne",
-  population: 5_000_000,
-}; // OK
+interface State {
+  currentPage: string;
+  requests: { [page: string]: RequestState };
+}
+
+function getUrlForPage(p: string) {
+  return "";
+}
+function renderPage(state: State) {
+  const { currentPage } = state;
+  const requestState = state.requests[currentPage];
+  switch (requestState.state) {
+    case "pending":
+      return `Loading ${currentPage}...`;
+    case "error":
+      return `Error! Unable to load ${currentPage}: ${requestState.error}`;
+    case "ok":
+      return `<h1>${currentPage}</h1>\n${requestState.pageText}`;
+  }
+}
+
+async function changePage(state: State, newPage: string) {
+  state.requests[newPage] = { state: "pending" };
+  state.currentPage = newPage;
+  try {
+    const response = await fetch(getUrlForPage(newPage));
+    if (!response.ok) {
+      throw new Error(`Unable to load ${newPage}: ${response.statusText}`);
+    }
+    const pageText = await response.text();
+    state.requests[newPage] = { state: "ok", pageText };
+  } catch (e) {
+    state.requests[newPage] = { state: "error", error: "" + e };
+  }
+}
+
+// Air France
+
+interface CockpitControls {
+  /** Angle of the left side stick in degrees, 0 = neutral, + = forward */
+  leftSideStick: number;
+  /** Angle of the right side stick in degrees, 0 = neutral, + = forward */
+  rightSideStick: number;
+}
+
+// Pilot in control
+function getStickSetting(controls: CockpitControls) {
+  return controls.leftSideStick;
+}
+
+// Assume copilot taken control when pilot stick is at 0
+function getStickSetting(controls: CockpitControls) {
+  const { leftSideStick, rightSideStick } = controls;
+  if (leftSideStick === 0) {
+    return rightSideStick;
+  }
+  return leftSideStick;
+}
+
+// Also check for copilot stick at 0
+function getStickSetting(controls: CockpitControls) {
+  const { leftSideStick, rightSideStick } = controls;
+  if (leftSideStick === 0) {
+    return rightSideStick;
+  } else if (rightSideStick === 0) {
+    return leftSideStick;
+  }
+  // ???
+}
+
+// What if both non-zero? When they are around the same position? Average them?
+function getStickSetting(controls: CockpitControls) {
+  const { leftSideStick, rightSideStick } = controls;
+  if (leftSideStick === 0) {
+    return rightSideStick;
+  } else if (rightSideStick === 0) {
+    return leftSideStick;
+  }
+  if (Math.abs(leftSideStick - rightSideStick) < 5) {
+    return (leftSideStick + rightSideStick) / 2;
+  }
+  // ???
+}
+
+// Air France 447
+function getStickSetting(controls: CockpitControls) {
+  return (controls.leftSideStick + controls.rightSideStick) / 2;
+}
+
+// Mechanically linked
+interface CockpitControls {
+  /** Angle of the stick in degrees, 0 = neutral, + = forward */
+  stickAngle: number;
+}
