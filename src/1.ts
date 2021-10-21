baec1aaa72a956235c933a9789a78ef991e247df
Raph <rsuttiyotin@seek.com.au>
2021-10-22 07:45:42 +1100

    Swap 1 and 2

--- src/1.ts
+++ src/1.ts
@@ -1,28 +1,3 @@
-// TypeScript compiles to JavaScript
-const add = (x: number, y: number) => x + y;
+const addNumbers = (a, b) => a + b;
 
-// TypeScript tries to detect code that will fail an exception at runtime without running the code
-// TypeScript can also detect errors as type annotations tell TypeScript what your intent is (see `interface` below)
-interface Candidate {
-  name: string;
-  title: "Principal Developer" | "Associate Developer";
-  favColour: string;
-}
-
-const candidates: Candidate[] = [
-  { name: "James", title: "Associate Developer", favColour: "green" },
-  { name: "John", title: "Principle Developer", favColor: "blue" },
-];
-
-// TypeScript models runtime behaviour of JavaScript
-const x = 2 + "3";
-const y = "2" + 3;
-
-const a = null + 7;
-const b = [] + 12;
-
-alert("Hello", "SEEK");
-
-// There are type checks that can still throw error at runtime
-const names = ["Alice", "Bob"];
-console.log(names[2].toUpperCase());
+const nullTest: number = null;
