67358cbe658924925fcab28320bccc77b9ef2b43
Raph <rsuttiyotin@seek.com.au>
2021-10-22 21:21:54 +1100

    Example

--- src/1.ts
+++ src/1.ts
@@ -1,3 +1,11 @@
 const addNumbers = (a, b) => a + b;
 
-const nullTest: number = null;
+declare const loggedInUsername: string;
+const users = [
+  { name: "Oby", age: 12 },
+  { name: "Heera", age: 32 },
+];
+
+const loggedInUser = users.find((u) => u.name === loggedInUsername);
+console.log(loggedInUser.age);
+// Object is possibly 'undefined'.
