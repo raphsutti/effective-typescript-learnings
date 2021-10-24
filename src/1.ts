6d544fcdb19966bff84099cefc905758a5082ce0
Raph <rsuttiyotin@seek.com.au>
2021-10-24 14:01:35 +1100

    comment out

--- src/1.ts
+++ src/1.ts
@@ -6,12 +6,12 @@
 const addNumbers = (a, b) => a + b;
 
 // strict null checks
-declare const loggedInUsername: string;
-const users = [
-  { name: "Oby", age: 12 },
-  { name: "Heera", age: 32 },
-];
+// declare const loggedInUsername: string;
+// const users = [
+//   { name: "Oby", age: 12 },
+//   { name: "Heera", age: 32 },
+// ];
 
-const loggedInUser = users.find((u) => u.name === loggedInUsername);
-console.log(loggedInUser.age);
-// Object is possibly 'undefined'.
+// const loggedInUser = users.find((u) => u.name === loggedInUsername);
+// console.log(loggedInUser.age);
+// // Object is possibly 'undefined'.
